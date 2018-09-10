6b12cb90726b919dda9829c66eb8a330adceb7d8
yash bhardwaj <yashbhardwaj95@gmail.com>
2018-09-10 13:42:02 +0530

    updated

--- RegisterLogin/Controllers/LoginController.cs
+++ RegisterLogin/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RegisterLogin.DAL;
 using RegisterLogin.Models;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Linq;
 
 namespace RegisterLogin.Controllers
 {
-
+    [Authorize]
     public class LoginController : Controller
     {
         private DBOperations dbOperations = new DBOperations();
